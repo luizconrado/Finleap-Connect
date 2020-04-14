e6c14ee23d6ce9ed6ae3f5a671b807cd5767f36d
Syed Furqan Ahmed <sonicfurqan@gmail.com>
2020-04-14 23:59:37 +0530

    Adding Filter to reports

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,76 @@
+declare module "@salesforce/label/c.Collapse_Text" {
+    var Collapse_Text: string;
+    export default Collapse_Text;
+}
+declare module "@salesforce/label/c.Expand_Text" {
+    var Expand_Text: string;
+    export default Expand_Text;
+}
+declare module "@salesforce/label/c.Insufficient_Access_Text" {
+    var Insufficient_Access_Text: string;
+    export default Insufficient_Access_Text;
+}
+declare module "@salesforce/label/c.Month_April" {
+    var Month_April: string;
+    export default Month_April;
+}
+declare module "@salesforce/label/c.Month_August" {
+    var Month_August: string;
+    export default Month_August;
+}
+declare module "@salesforce/label/c.Month_December" {
+    var Month_December: string;
+    export default Month_December;
+}
+declare module "@salesforce/label/c.Month_February" {
+    var Month_February: string;
+    export default Month_February;
+}
+declare module "@salesforce/label/c.Month_January" {
+    var Month_January: string;
+    export default Month_January;
+}
+declare module "@salesforce/label/c.Month_July" {
+    var Month_July: string;
+    export default Month_July;
+}
+declare module "@salesforce/label/c.Month_June" {
+    var Month_June: string;
+    export default Month_June;
+}
+declare module "@salesforce/label/c.Month_March" {
+    var Month_March: string;
+    export default Month_March;
+}
+declare module "@salesforce/label/c.Month_May" {
+    var Month_May: string;
+    export default Month_May;
+}
+declare module "@salesforce/label/c.Month_November" {
+    var Month_November: string;
+    export default Month_November;
+}
+declare module "@salesforce/label/c.Month_October" {
+    var Month_October: string;
+    export default Month_October;
+}
+declare module "@salesforce/label/c.Month_September" {
+    var Month_September: string;
+    export default Month_September;
+}
+declare module "@salesforce/label/c.Restricted_Edit_Upload_Files_Objects" {
+    var Restricted_Edit_Upload_Files_Objects: string;
+    export default Restricted_Edit_Upload_Files_Objects;
+}
+declare module "@salesforce/label/c.Upcoming_Overdue" {
+    var Upcoming_Overdue: string;
+    export default Upcoming_Overdue;
+}
+declare module "@salesforce/label/c.Users_Exclusion" {
+    var Users_Exclusion: string;
+    export default Users_Exclusion;
+}
+declare module "@salesforce/label/c.View_All_Text" {
+    var View_All_Text: string;
+    export default View_All_Text;
+}
